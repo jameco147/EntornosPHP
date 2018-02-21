6e4845bc61204878cebedc58a118835f478860b5
Javi <jmerinocoll@gmail.com>
2018-02-21 09:42:27 +0100

    Batalla con formulario

---
+++ Batallayformulario/batalla_informacion.php
@@ -0,0 +1,37 @@
+<?php
+define("MIN",32);
+define("MAX", 122);
+$fmove = 16;
+//$input_string = "Una cosa es ser solidario, y otra es serlo a cambio de nada. Mariano Rajoy";
+$input_string = $_POST['caja'];
+$input_length = strlen($input_string);
+$input_array = [];
+for ($i=0; $input_length > $i ; $i++) {
+  $char = $input_string[$i];
+  $char_val = ord($char);
+  if ($char_val + $fmove > MAX) {
+    $a = $char_val + $fmove - MAX;
+    $result = MIN - 1 + $a;
+  } else {
+    $result = $char_val + $fmove;
+  }
+
+  if ($result > MAX) {
+    $a = $result - MAX;
+    $result = MIN - 1 + $a;
+  }
+  $input_array[] = $result;
+  $fmove = $result;
+}
+// Muestra el código numérico ascii de cada caracter
+$numbers = implode(",", $input_array);
+//echo $numbers . "<br><br>";
+// Esto muestra los caracteres que entregaremos al resto de compañeros
+for ($i=0; $i < $input_length ; $i++) {
+  $ascii = $input_array[$i];
+  echo "<b>" . chr($ascii) . "</b>";
+
+}
+?>
+<br><br><br>
+<a href = "formulario.php">Volver</a>
