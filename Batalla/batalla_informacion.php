e2b92e9f0660a57c3261f4d0db477cc9675c7677
Javi <jmerinocoll@gmail.com>
2018-02-21 09:50:19 +0100

    Modificado

--- Batalla/batalla_informacion.php
+++ Batalla/batalla_informacion.php
@@ -1,45 +1,37 @@
 <?php
-$min = 32;
-$max = 122;
+define("MIN",32);
+define("MAX", 122);
 $fmove = 16;
-$input_string ="Una cosa es ser solidario, y otra es serlo a cambio de nada. Mariano Rajoy";
-//$input_string = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+//$input_string = "Una cosa es ser solidario, y otra es serlo a cambio de nada. Mariano Rajoy";
+$input_string = $_POST['caja'];
 $input_length = strlen($input_string);
-
 $input_array = [];
-
 for ($i=0; $input_length > $i ; $i++) {
   $char = $input_string[$i];
   $char_val = ord($char);
-
-  if ($char_val + $fmove > $max) {
-    $a = $char_val + $fmove - $max;
-    $result = $min - 1 + $a;
-
-//el siguiente if es por si supera los 122 en la primera pasada.
-    if ($result > $max) {
-      $a = $result - $max;
-      $result = $min - 1 + $a;
-    }
-    $input_array[] = $result;
+  if ($char_val + $fmove > MAX) {
+    $a = $char_val + $fmove - MAX;
+    $result = MIN - 1 + $a;
   } else {
     $result = $char_val + $fmove;
-    $input_array[] = $result;
-
   }
 
+  if ($result > MAX) {
+    $a = $result - MAX;
+    $result = MIN - 1 + $a;
+  }
+  $input_array[] = $result;
   $fmove = $result;
-
 }
-
-//esto muestra el código numérico ascii de cada caracter
+// Muestra el código numérico ascii de cada caracter
 $numbers = implode(",", $input_array);
-echo $numbers . "<br><br>";
-
-//esto muestra los caracteres que entregaremos al resto de compañeros
+//echo $numbers . "<br><br>";
+// Esto muestra los caracteres que entregaremos al resto de compañeros
 for ($i=0; $i < $input_length ; $i++) {
-  $ascii=$input_array[$i];
+  $ascii = $input_array[$i];
   echo "<b>" . chr($ascii) . "</b>";
 
 }
 ?>
+<br><br><br>
+<a href = "formulario.php">Volver</a>
---
+++ Batalla/formulario.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Batalla información</title>
+    <style>
+
+      #color:hover {
+        color: rgb(73, 76, 208);
+      }
+
+    </style>
+  </head>
+  <body>
+    <h2>Batalla de la información</h2>
+    <form action="batalla_informacion.php" method="post">
+      <input type="text" placeholder="Introduce texto:" name="caja">
+      <input type="submit" placeholder="Codificar:" value="Codificar" id="color">
+    </form>
+  </body>
+</html>
